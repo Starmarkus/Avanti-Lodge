ec85582d0478d5942b1e0080f9bbf812605d4ce6
Matt-Joe <matthew.joseph.xpndble@gmail.com>
2025-05-20 10:39:18 +0200

    self catering complete

--- Accomodation/Self Catering/selfcatering.js
+++ Accomodation/Self Catering/selfcatering.js
@@ -1,37 +1,37 @@
-// selfcatering.js
+document.addEventListener('DOMContentLoaded', () => {
+  const bg1 = document.getElementById('bg1');
+  const bg2 = document.getElementById('bg2');
 
-const images = [
-  '/Images/Rooms/Flat 1/bathroom.jpg',
-  '/Images/Rooms/Flat 1/bedroom.jpg',
-  '/Images/Rooms/Flat 1/kitchen.jpg',
-  '/Images/Rooms/Flat 3/chairs.jpg',
-];
+  const images = [
+    '/Images/Rooms/Flat 1/kitchen.jpg',
+    '/Images/Rooms/Flat 1/bedroom.jpg',
+    '/Images/Rooms/Flat 1/bathroom.jpg',
+    '/Images/Rooms/Flat 3/chairs.jpg'
+  ];
 
-let index = 0;
-let showingBg1 = true;
+  let currentIndex = 0;
+  let showingBg1 = true;
 
-const bg1 = document.getElementById('bg1');
-const bg2 = document.getElementById('bg2');
+  // Set initial images
+  bg1.style.backgroundImage = `url('${images[0]}')`;
+  bg2.style.backgroundImage = `url('${images[1]}')`;
+  bg1.style.opacity = '1';
+  bg2.style.opacity = '0';
 
-// Initial background
-bg1.style.backgroundImage = `url('${images[0]}')`;
-bg1.style.opacity = 1;
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    const nextIndex = (currentIndex + 1) % images.length;
 
-setInterval(() => {
-  index = (index + 1) % images.length;
-  const nextImage = images[index];
+    if (showingBg1) {
+      bg2.style.backgroundImage = `url('${images[nextIndex]}')`;
+      bg2.style.opacity = '1';
+      bg1.style.opacity = '0';
+    } else {
+      bg1.style.backgroundImage = `url('${images[nextIndex]}')`;
+      bg1.style.opacity = '1';
+      bg2.style.opacity = '0';
+    }
 
-  if (showingBg1) {
-    bg2.style.backgroundImage = `url('${nextImage}')`;
-    bg2.style.opacity = 1;
-    bg1.style.opacity = 0;
-  } else {
-    bg1.style.backgroundImage = `url('${nextImage}')`;
-    bg1.style.opacity = 1;
-    bg2.style.opacity = 0;
-  }
-
-  showingBg1 = !showingBg1;
-}, 5000); // every 5 seconds
-
-  
+    showingBg1 = !showingBg1;
+  }, 5000);
+});
