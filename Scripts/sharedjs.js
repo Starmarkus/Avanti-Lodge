995eb71e50e9aa8a9b692f68feb86cdba4a348c9
LittleGiant476 <102793122+LittleGiant476@users.noreply.github.com>
2025-05-21 10:52:54 +0200

    Reworked the Navbar for reactivness

--- Scripts/sharedjs.js
+++ Scripts/sharedjs.js
@@ -6,3 +6,18 @@
       nav.classList.remove('scroll-active');
     }
   });
+
+ 
+  const burgerButton = document.querySelector('.burger-button');
+  const burgerDropdown = document.querySelector('.burger-dropdown');
+
+  burgerButton.addEventListener('click', () => {
+    burgerDropdown.style.display = burgerDropdown.style.display === 'block' ? 'none' : 'block';
+  });
+
+  // Optional: click outside to close
+  window.addEventListener('click', (e) => {
+    if (!e.target.matches('.burger-button')) {
+      burgerDropdown.style.display = 'none';
+    }
+  });
